dce04a7952aa011429aae31d714a88dbc66189c6
KathLouise <kathe.nightwish@gmail.com>
2016-08-24 11:20:46 -0300

    WIP:Validação de IP e porta mais rigida
    Da pra melhorar ainda.

--- port_scanner.c
+++ port_scanner.c
@@ -10,7 +10,7 @@
 #include <ctype.h>
 
 #define MAX_IP 16
-#define MAX_PORT 66000
+#define MAX_PORT 65536
 
 int validateIPAddr(char *ip){
     struct sockaddr_in socket;
@@ -46,10 +46,11 @@ void createToken(char *range[],  char string[], const char delimiter[], int *con
     *cont = i;
 }
 
-void constructIPRange(char range_ipSeq[], char *range_ip[], char *rangeTotal[], int *numIP){
+void constructIPRange(char range_ipSeq[], char *rangeTotal[], int *numIP){
     const char delimiter[2] = "-";
     const char delimiter2[2] = ".";
     char *token, *token2, *addr;
+    char *range_ip[MAX_IP] = {0};
     char *range[MAX_IP] = {0};
     char *range2[MAX_IP] = {0};
     char first_ip[MAX_IP] = {0};
@@ -127,17 +128,22 @@ void constructPortRange(char range_portSeq[], int range[], int *numPort){
    
     /* walk through other tokens */
     while(token != NULL){
-        port[i] = atoi(token);
-        if(!isdigit(port[i])){
+        if(!isdigit(*token)){
             printf("Range de porta invalida.\n");
             exit(1);
         }
+        port[i] = atoi(token);
         token = strtok(NULL, delimiter);
         i++;
     }
     
     init = port[0];
     final = port[1];
+    
+    if(init > MAX_PORT || final > MAX_PORT){
+        printf("Range de porta extrapolou o valor permitido.\n");
+        exit(1);
+    }
 
     *numPort = final - init;
     for(i = init; i <= final; i++){
@@ -147,50 +153,99 @@ void constructPortRange(char range_portSeq[], int range[], int *numPort){
     
 }
 
+int connectIP(int port_number, char *ip){
+    struct sockaddr_in server_addr;
+    int sock, try_connect;
+
+    //cria o socket
+    sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    
+    if(sock < 0){
+        printf("Erro ao criar o socket.\n");
+        exit(1);    
+    }
+    printf("1\n");
+    server_addr.sin_addr.s_addr = inet_addr(ip); //IP do server, neste caso localhost
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(port_number);
+
+    //Conectando ao server
+    int i = 0;
+    try_connect = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    if(try_connect < 0){
+        printf("Erro ao tentar conectar. \n");
+        perror("");
+    }
+
+    return sock;
+}
+
+
 void main(int argc, char *argv[]){
     char range_ipSeq[100];
     char range_portSeq[100];
-    char *range_ip[MAX_IP] = {0};
     char *rangeTotal[256] = {0};
+    char *isIPRange, *isPortRange;
     char first_port[MAX_PORT] = {0};
     int range_port[MAX_PORT];
-    int numPort = 0, numIP = 0, i, j, isValidIP, hasIPRange = 1, hasPortRange = 1;
-    char *isIPRange, *isPortRange;
+    int numPort = 0, numIP = 0, i, j, isValidIP, hasIPRange = 1, hasPortRange = 1, sock;
 
     if(argc < 3){
-        printf("Entrada incorreta.\n\n A entrada deve seguir o seguinte modelo: ./portscanner <ip ou range de ips> <porta ou range de portas>\n");
+        printf("Entrada incorreta.\n\n");
+        printf("A entrada deve seguir o seguinte modelo: ./portscanner <ip ou range de ips> <porta ou range de portas>\n");
         exit(0);
     }
 
     strcpy(range_ipSeq, argv[1]);
     strcpy(range_portSeq, argv[2]);
-    
-    
-    
+
     isIPRange = strchr (range_ipSeq, '-');
     if(isIPRange != NULL){
-        constructIPRange(range_ipSeq, range_ip, rangeTotal, &numIP);
+        constructIPRange(range_ipSeq, rangeTotal, &numIP);
     }else{
-        rangeTotal[0] = range_ipSeq;
-        numIP = 1;
+        if(!isdigit(range_ipSeq[1])){
+            if(range_ipSeq[1] != '\0'){
+                rangeTotal[0] = range_ipSeq;
+                numIP = 1;
+            }
+        }else{
+            printf("Caracter de separação invalido, ou range desconhecido. \n");
+            printf("Tente novamente, separando seu range por - .\n");
+            exit(1);
+        }
     }
     
     isPortRange = strchr (range_portSeq, '-');
     if(isPortRange != NULL){
         constructPortRange(range_portSeq, range_port, &numPort);
     }else{
-        printf("token: %d\n", range_port[0]);
-        if(!isdigit(range_port[0])){
-            printf("Porta inválida.\n");
+        if(isdigit(range_portSeq[1])){
+            if(!isdigit(range_portSeq[0])){
+                printf("Porta inválida.\n");
+                exit(1);
+            }
+            if(atoi(range_portSeq) > MAX_PORT){
+                printf("Range de porta extrapolou o valor permitido.\n");
+                exit(1);
+            }
+            range_port[0] = atoi(range_portSeq);
+            numPort = 1;
+        }else{
+            printf("Caracter de separação invalido, ou range desconhecido. \n");
+            printf("Tente novamente, separando seu range por - .\n");
             exit(1);
         }
-        numPort = 1;
     }
 
     for(i = 0; i < numIP; i++){
         isValidIP = validateIPAddr(rangeTotal[i]);
         if(isValidIP){
-            printf("IP válido.\n");
+            for(j = 0; j < numPort; j++){
+                printf("conectando =).\n");
+                printf("port.%d\n", range_port[j]);
+               // sock = connectIP(range_port[j], rangeTotal[i]);
+            }
         }else{
             printf("IP inválido.\n");
         }
